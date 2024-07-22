6854b846274b91cd9bb04328d8867af88323df8f
Evgeny Volenko <evolenko@inbox.ru>
2024-07-22 17:01:49 +0300

    added cw_6 0.5

---
+++ CW_6/code/src/Domain/Models/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Geekbrains\Application1\Domain\Models;
+
+use Geekbrains\Application1\Application\Application;
+
+class User
+{
+
+    private ?int $idUser;
+    private ?string $userName;
+    private ?string $userLastName;
+    private ?int $userBirthday;
+
+    // private static string $storageAddress = '/storage/birthdays.txt';
+
+    public function __construct(string $name = null, string $lastName = null, int $birthday = null, int $id_user = null)
+    {
+        $this->userName = $name;
+        $this->userLastName = $lastName;
+        $this->userBirthday = $birthday;
+        $this->idUser = $id_user;
+    }
+    public function setUserId(int $id_user): void
+    {
+        $this->idUser = $id_user;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->idUser;
+    }
+
+    public function setName(string $userName): void
+    {
+        $this->userName = $userName;
+    }
+
+    public function setLastName(string $userLastName): void
+    {
+        $this->userLastName = $userLastName;
+    }
+
+    public function getUserName(): string
+    {
+        return $this->userName;
+    }
+
+    public function getUserLastName(): string
+    {
+        return $this->userLastName;
+    }
+
+    public function getUserBirthday(): int
+    {
+        return $this->userBirthday;
+    }
+
+    public function setBirthdayFromString(string $birthdayString): void
+    {
+        $this->userBirthday = strtotime($birthdayString);
+    }
+
+    public static function getAllUsersFromStorage(): array
+    {
+        $sql = "SELECT * FROM users";
+
+        $handler = Application::$storage->get()->prepare($sql);
+        $handler->execute();
+        $result = $handler->fetchAll();
+
+        $users = [];
+
+        foreach ($result as $item) {
+            $user = new User($item['user_name'], $item['user_lastname'], $item['user_birthday_timestamp']);
+            $users[] = $user;
+        }
+
+        return $users;
+    }
+
+    public static function validateRequestData(): bool
+    {
+        if (
+            isset($_GET['name']) && !empty($_GET['name']) &&
+            isset($_GET['lastname']) && !empty($_GET['lastname']) &&
+            isset($_GET['birthday']) && !empty($_GET['birthday'])
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setParamsFromRequestData(): void
+    {
+        $this->userName = $_GET['name'];
+        $this->userLastName = $_GET['lastname'];
+        $this->setBirthdayFromString($_GET['birthday']);
+    }
+
+    public function saveToStorage()
+    {
+        $sql = "INSERT INTO users(user_name, user_lastname, user_birthday_timestamp) VALUES (:user_name, :user_lastname, :user_birthday)";
+
+        $handler = Application::$storage->get()->prepare($sql);
+        $handler->execute([
+            'user_name' => $this->userName,
+            'user_lastname' => $this->userLastName,
+            'user_birthday' => $this->userBirthday
+        ]);
+    }
+}
